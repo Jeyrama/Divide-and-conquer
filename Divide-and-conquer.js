39814cde26a137401742f35344d4143d4004c74f
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-08 11:03:34 -0700

    Update Divide-and-conquer.js

--- Divide-and-conquer.js
+++ Divide-and-conquer.js
@@ -12,3 +12,17 @@ function divCon(x) {
 }
 
 // or
+
+function divCon(x) {
+  let numValue = 0;
+  let strValue = 0;
+
+  for(let i = 0; i < x.length; i++) {
+    if(typeof(x[i]) === 'string') {
+      strValue += parseFloat(x[i]);
+    } else if(typeof(x[i]) === 'number' ) {
+      numValue += x[i];
+    }
+  }
+  return numValue - strValue;
+}
